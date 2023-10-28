af44ea5fde5d7bf89cc7272f3a00b948a3a5ab9e
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2023-10-28 04:31:24 +0100

    saturday

--- src/CityData.js
+++ src/CityData.js
@@ -4,7 +4,7 @@ import React from 'react';
 const CityData = ({ city, temperature, airQuality }) => {
   return (
     <div>
-      <h2>{city}</h2>
+      <h2 className='cityname'>{city}</h2>
       <p className='text'>AVERAGE TEMPERATURE: {temperature}°C</p>
       <p className='text'>AIR QUALITY (PM2.5): {airQuality}</p>
     </div>
