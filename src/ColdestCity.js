ecb8850feadd34dcf492e5cab6b99de9f726c112
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2023-10-30 17:36:29 +0000

    Update ColdestCity.js

--- src/ColdestCity.js
+++ src/ColdestCity.js
@@ -7,15 +7,14 @@ const TopTenColdestCities = () => {
   const sortedCitiesByTemperature = updatedCitiesData.sort((city1, city2) => city1.temperature - city2.temperature);
 
   // Get the top 10 Coldest cities
-  const topTenColdestCities = sortedCitiesByTemperature.slice(0, 10);
-
+  const topTenColdestCities = sortedCitiesByTemperature.slice(0, 5);
   return (
     <div>
-      <h2>Top 10 Coldest Cities</h2>
+      <h2>The Top 10 Coldest Cities</h2>
       <ul>
         {topTenColdestCities.map((city, index) => (
           <li key={index}>
-            {`${index + 1}. ${city.name} - Temperature: ${city.temperature}°C : ${city.airQuality}PM2.5`}
+            {`${index + 0}. ${city.name} - Temperature: ${city.temperature}°C : ${city.airQuality}PM2.5`}
           </li>
         ))}
       </ul>
